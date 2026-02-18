fcf996029a6f17b8703ecdd92fbdd081007c517f
Emma-267 <ek3833@srmist.edu.in>
2026-02-18 16:06:10 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,8 @@
+//Display a welcome message and application information when the Palindrome Checker App starts.
+public class UseCase1PalindromeCheckerApp{
+    public static void main(String[] args){
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version: 1.0");
+        System.out.println("System initialized successfully");
+    }
+}
