897165c8217c02029fc46d0aa18cde61a642d0d8
Emma-267 <ek3833@srmist.edu.in>
2026-02-24 09:21:58 +0530

    UC11: Object-Oriented Palindrome Service

---
+++ src/UseCase11PalindromeCheckerApp.java
@@ -0,0 +1,23 @@
+//Encapsulate palindrome logic in a class.
+class PalindromeService{
+    public static boolean checkPalindrome(String input){
+        int start=0;
+        int end=input.length()-1;
+        while(start<end){
+            if (input.charAt(start)!=input.charAt(end)){
+                return false;
+            }
+            end--;
+            start++;
+        }
+        return true;
+    }
+}
+public class UseCase11PalindromeCheckerApp{
+    public static void main(String[] args){
+        String word="racecar";
+        boolean palindrome=PalindromeService.checkPalindrome(word);
+        System.out.println("Input text: "+word);
+        System.out.println("Is it a palindrome?: "+palindrome);
+    }
+}
